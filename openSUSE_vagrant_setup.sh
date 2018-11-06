260049dbe9ee4a1af54e5902e045850ee0c1b386
Denis Kondratenko <denis.kondratenko@suse.com>
2018-11-06 12:41:00 +0100

    updating vagrant to 2.2.0

--- openSUSE_vagrant_setup.sh
+++ openSUSE_vagrant_setup.sh
@@ -5,9 +5,9 @@ set -ex
 zypper_version=($(zypper -V))
 if [[ ${zypper_version[1]} < '1.14.4' ]]
 then
-    zypper --no-gpg-checks in -y https://releases.hashicorp.com/vagrant/2.1.5/vagrant_2.1.5_x86_64.rpm
+    zypper --no-gpg-checks in -y https://releases.hashicorp.com/vagrant/2.2.0/vagrant_2.2.0_x86_64.rpm
 else
-    zypper in -y --allow-unsigned-rpm https://releases.hashicorp.com/vagrant/2.1.5/vagrant_2.1.5_x86_64.rpm
+    zypper in -y --allow-unsigned-rpm https://releases.hashicorp.com/vagrant/2.2.0/vagrant_2.2.0_x86_64.rpm
 fi
 
 # workaround for https://github.com/hashicorp/vagrant/issues/10019
